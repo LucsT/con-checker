6b6ae1465d374f2620c62901abcf8be20959bf13
Lucs_T <tosh@tosh.(none)>
2013-02-08 01:34:35 +0100

    Test ok

--- con-checker.py
+++ con-checker.py
@@ -3,63 +3,74 @@ import datetime
 
 
 IPONLINE1="8.8.8.8"
-IPONLINE2="X"
+IPONLINE2="89.2.0.1"
 IPROUTER="192.168.0.1"
+LOG_FILE="statut.log"
+# Delay between ping in seconds
+DELAY="5"
 
+def check_ip1():
+    return os.system("ping -c 1 %s >> /dev/null" % IPONLINE1)
 
-def check_ip1:
-    return os.system("ping -c 1 %s >> /dev/null", % IPONLINE1)
+def check_ip2():
+    return os.system("ping -c 1 %s >> /dev/null" % IPONLINE2)
 
-def check_ip2:
-    return os.system("ping -c 1 %s >> /dev/null", % IPONLINE2)
+def check_router():
+    return os.system("ping -c 1 %s >> /dev/null" % IPROUTER)
 
-try:
-    file = open("status.log","a")
-except:
-    file = open("status.log","w")
+def log(string):
+      print(string)
+      try:
+          file = open(LOG_FILE,"a")
+      except:
+          file = open(LOG_FILE,"w")
+      file.write(string)
+      file.close()
+
+def wait_ip1_online():
+    while check_ip1:
+        os.system("sleep %s" % DELAY)
+    
+
+#Start message with a blank line
+big_delay = 0
 now = datetime.datetime.now()
-file.write("\nStart at : %s\n" % now.strftime("%Y-%m-%d %H:%M:%S"))
-file.close()
-
+log(" \nStart at : %s\n" % now.strftime("%Y-%m-%d %H:%M:%S"))
 
 while(1):
-    #Si on perds google
     if check_ip1():
-        #Ouverture du fichier log
-        file = open("status.log","a")
         down = datetime.datetime.now()
-        #On verifie si uniquement google est down
-        if not os.system("ping -c 1 %s >> /dev/null", % IPONLINE2):
-            print("GOOGLE seems down!!")
-            file.write("%s GOOGLE seems down!!\n" % down.strftime("%Y-%m-%d %H:%M:%S"))
+        #If ip1 is out, check ip2
+        if not check_ip2():
+            log("%s GOOGLE seems down!!\n" % down.strftime("%Y-%m-%d %H:"\
+                                                               + "%M:%S"))
         #Temporisation
         os.system("sleep 1")
-        # Si google out et box OK
-        if os.system("ping -c 1 %s >> /dev/null", % IPONLINE1) and not os.system("ping -c 1 %s >> /dev/null", % IPROUTER):
-            print("DOWN at %s" % down.strftime("%Y-%m-%d %H:%M:%S"))
-            file.write("%s DOWN\n" % down.strftime("%Y-%m-%d %H:%M:%S"))
-            #Attendre le retour de google
-            while os.system("ping  -c 1 8.8.8.8 >> /dev/null"):
-                os.system("sleep 5")
+        #If you're still reach your router, connection problem
+        if check_ip1() and not check_router():
+            log("%s DOWN\n" % down.strftime("%Y-%m-%d %H:%M:%S"))
+            wait_ip1_online()
             up = datetime.datetime.now()
             delay = (up - down)
-            file.write("%s UP \n" % up.strftime("%Y-%m-%d %H:%M:%S"))
-            print("UP at %s after %s Seconds" % (up.strftime("%Y-%m-%d %H:%M:%S", delay.seconds)))
-        # Si google out et box out
-        elif os.system("ping -c 1 8.8.8.8 >> /dev/null") and os.system("ping -c 1 192.168.0.1 >> /dev/null"):
-            file.write("%s LAN UNREACHABLE\n" % down.strftime("%Y-%m-%d %H:%M:%S"))
-            while os.system("ping  -c 1 8.8.8.8 >> /dev/null"):
-                #Do nothing, wait etablishement
-                os.system("sleep 5")
-            now = datetime.datetime.now()
-            file.write("%s UP \n" % now.strftime("%Y-%m-%d %H:%M:%S"))
-            print("UP at %s"  % now.strftime("%Y-%m-%d %H:%M:%S"))
+            log("%s UP after %s \n" %(up.strftime("%Y-%m-%d %H:%M:%S"),
+                                      delay.strftime("%H:%M:%S")))
+        #If you can't contact your router, it's your network problem
+        elif check_ip1() and check_router():
+            log("%s UNREACHABLE\n" % down.strftime("%Y-%m-%d %H:%M:%S"))
+            wait_ip1_online()
+            up = datetime.datetime.now()
+            delay = (up - down)
+            log("%s UP after %s \n" %(up.strftime("%Y-%m-%d %H:%M:%S"),
+                                      delay.strftime("%H:%M:%S")))
+        #else it's just a lost packet
         else:
-            file.write("%s 1 packet loss\n" % now.strftime("%Y-%m-%d %H:%M:%S"))    
-        
-        file.close()
-
-    print("UP %s" % datetime.datetime.now())
+            log("%s 1 packet loss\n" % now.strftime("%Y-%m-%d %H:%M:%S"))    
+    
+    big_delay -= 1
+    if big_delay < 0:
+        big_delay = 600
+        now = datetime.datetime.now()
+        log("%s UP \n" % now.strftime("%Y-%m-%d %H:%M:%S"))
+    #Wait the delay and go on!
     os.system("sleep 5")
 
-file.close()
